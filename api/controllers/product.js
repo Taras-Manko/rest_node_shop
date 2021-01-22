db3f73bae1ad3df77cbf5413c2a04000385bab02
Taras Man'ko <manko5613@gmail.com>
2021-01-22 19:19:02 +0300

    photo

--- api/controllers/product.js
+++ api/controllers/product.js
@@ -1,9 +1,11 @@
 const Product = require('../models/product')
+const User = require('../models/user')
 const multer = require('multer')
 const {
     storage,
     fileFilter
 } = require('../../middleware/file')
+const { findByIdAndUpdate } = require('../models/product')
 
 const getProducts = async (req, res) => {
     await Product.find()
@@ -31,12 +33,13 @@ const getProducts = async (req, res) => {
 }
 
 const postProducts = async (req, res) => {
-
-    const product = new Product({
+    console.log(req.userData.userId)
+    let product = new Product({
         name: req.body.name,
         price: req.body.price,
-        user:req.userData
+        user:req.userData.userId
     })
+    product = await User.populate(product,{path : 'user'})
     await product.save((err, prod) => {
         if (err) {
             console.log(err)
@@ -52,11 +55,11 @@ const postProducts = async (req, res) => {
 }
 
 const postProductsImages = async (req, res) => {
-    const user = await findById(req.params.id)
-    if(user) {
-        user = new Product({
+   let product = await Product.findById(req.params.id)
+   product = new Product({
             productImage: req.file.path
         })
+
         await product.save((err, prod) => {
             if (err) {
                 console.log(err)
@@ -68,11 +71,7 @@ const postProductsImages = async (req, res) => {
                 })
             }
         })
-    } else {
-        res.status(404).json({
-            message:"User is not found"
-        })
-    }
+    
     
 
 }
@@ -90,14 +89,14 @@ const getIdProducts = async (req, res) => {
 }
 
 const updateProducts = async (req, res) => {
-    const {title} = req.body
-        if(title.value.length < 3 ) {
+    const {name} = req.body
+        if(name.length < 3 ) {
             res.sendStatus(422).json({
                 message:"Title should contain at least 3 characters"
             })
         }
     try {
-        const items = await Product.findOneAndUpdate(req.params.id, req.body)
+        const items = await Product.findByIdAndUpdate(req.params.id, req.body)
         .populate('user')
         res.status(200).json(items)
 
@@ -108,7 +107,7 @@ const updateProducts = async (req, res) => {
 }
 
 const deletedProduct = async (req, res) => {
-    await Product.findOneAndDelete({
+    await Product.findByIdAndDelete({
             _id: req.params.id
         }).exec()
         .then(del => {
--- middleware/token.js
+++ middleware/token.js
@@ -5,7 +5,9 @@ const key = require('../config/app')
 
 module.exports = (req,res,next) => {
     try {
+        
         const token = req.headers.authorization.split(' ')[1]
+        
         const decode = jwt.verify(token,key.JWT)
         req.userData = decode
         next()
